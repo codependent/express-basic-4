a2515272a9a07f41c09179fc404522171ed76625
codependent <joseantonio.inigo@gmail.com>
2014-04-25 13:27:02 +0200

    commit6

--- test/mocha-should-basic.js
+++ test/mocha-should-basic.js
@@ -1,12 +1,14 @@
 var assert = require("assert")
-var should = require('should');
 
 describe('Array', function(){
 	describe('#indexOf()', function(){
 		it('should return -1 when the value is not present', function(){
+			//Con assert
 			assert.equal(-1, [1,2,3].indexOf(5));
-			assert.equal(-1, [1,2,3].indexOf(0));
-			[1,2,3].indexOf(5).should.be.exactly(-1);
+			assert.equal(1, [1,2,3].indexOf(2));
+			//Equivalente con should
+			[1,2,3].indexOf(5).should.be.exactly(-1);			
+			[1,2,3].indexOf(2).should.be.exactly(1);
 		})
 	})
 });
